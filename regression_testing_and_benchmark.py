0a2120905932944f791197f8897be4aa538e8d9c
Kanegae <gabriel.kanegae@usp.br>
2020-07-10 19:54:54 -0300

    Fix for Windows

--- regression_testing_and_benchmark.py
+++ regression_testing_and_benchmark.py
@@ -30,29 +30,30 @@ def test(cleanImageFilename, payloadFilename):
     os.remove(encodedImageFilename)
     os.remove(payloadFilename.split("/")[-1])
 
-test("demo_files/corgi-599x799.jpg", "demo_files/payloads/faustao.png") # L0
-'''
-Encoding 'faustao.png' into 'demo_files/corgi-599x799.jpg' using L0...
-Encoding took 0.277s.
-File 'faustao.png' found encoded as L0!
-Decoding...
-Decoding took 0.505s.
-'''
+if __name__ == "__main__":
+    test("demo_files/corgi-599x799.jpg", "demo_files/payloads/faustao.png") # L0
+    '''
+    Encoding 'faustao.png' into 'demo_files/corgi-599x799.jpg' using L0...
+    Encoding took 0.277s.
+    File 'faustao.png' found encoded as L0!
+    Decoding...
+    Decoding took 0.505s.
+    '''
 
-test("demo_files/nightfall-1920x1080.jpg", "demo_files/payloads/hap.mp4") # L1
-'''
-Encoding 'hap.mp4' into 'demo_files/nightfall-1920x1080.jpg' using L1...
-Encoding took 2.018s.
-File 'hap.mp4' found encoded as L1!
-Decoding...
-Decoding took 2.633s.
-'''
+    test("demo_files/nightfall-1920x1080.jpg", "demo_files/payloads/hap.mp4") # L1
+    '''
+    Encoding 'hap.mp4' into 'demo_files/nightfall-1920x1080.jpg' using L1...
+    Encoding took 2.018s.
+    File 'hap.mp4' found encoded as L1!
+    Decoding...
+    Decoding took 2.633s.
+    '''
 
-test("demo_files/randall-2560x1372.png", "demo_files/payloads/pier39.mp4") # L2
-'''
-Encoding 'pier39.mp4' into 'demo_files/randall-2560x1372.png' using L2...
-Encoding took 2.919s.
-File 'pier39.mp4' found encoded as L2!
-Decoding...
-Decoding took 3.686s.
-'''
+    test("demo_files/randall-2560x1372.png", "demo_files/payloads/pier39.mp4") # L2
+    '''
+    Encoding 'pier39.mp4' into 'demo_files/randall-2560x1372.png' using L2...
+    Encoding took 2.919s.
+    File 'pier39.mp4' found encoded as L2!
+    Decoding...
+    Decoding took 3.686s.
+    '''
